36e8de8e68767fda18ef6641e9d0402d77b59594
Haciyev ilkin <ilkinhaciyev955@gmail.com>
2023-04-21 15:29:23 +0400

    multistep

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import Main from "./pages/Main";
 import Form from "./pages/newlogin";
+import MultiCreditForm from './pages/MultiCreditForm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/new-login" element={<Form />} />
+        <Route path="/credit" element={<MultiCreditForm />} /> 
       </Routes>
       <Footer />
     </>
